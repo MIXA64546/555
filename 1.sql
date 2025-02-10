79e1c70ef8e98b5cf9db472877962dda827de2d9
Filippov Mikhail <m1sha64546@yandex.ru>
2025-02-10 13:16:00 +0300

    777

--- 1.sql
+++ 1.sql
@@ -7,5 +7,5 @@ Select Prepodovatel.Familiya as F_prepod,
 		Student.Familiya as S_familya,
 		Student.Name as S_name,
 		Student.Othestvo as S_othestvo
-		from Prepodovatel,Ocenka,Student,Disciplina_Prepod
+		from Prepodovatel,Ocenka,Student,Disciplina_Prepodavatel
 		where Disciplina_Prepodovatel.DP_id = Ocenka.Code_PD and Student.Student_id = Ocenka.Cod_studenta and Prepodovatel.Prepodovatel_id = Disciplina_Prepodovatel.Prepodovatel_cod
